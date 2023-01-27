2e670052b2a8349a93f9f19f710487fa53f48a58
Jason Chen <jason.ccfu@gmail.com>
2023-01-26 16:57:04 -0800

    update db file

--- db/schema.sql
+++ db/schema.sql
@@ -27,3 +27,7 @@ CREATE TABLE employee (
   FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES employee(id)
   );
+
+SELECT * FROM department;
+SELECT * FROM employee_role;
+SELECT * FROM employee;
